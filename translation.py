81a9f31ff7594a03daf784ac49df89b71ca73c68
Aryana Mohammadi <am5723@barnard.edu>
2024-03-21 02:08:55 -0400

    translation logic

---
+++ translation.py
@@ -0,0 +1,39 @@
+# Morse code dictionary
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....',
+    '7': '--...', '8': '---..', '9': '----.',
+    '.': '.-.-.-', ',': '--..--', '?': '..--..', "'": '.----.', '!': '-.-.--', '/': '-..-.', '(': '-.--.',
+    ')': '-.--.-', '&': '.-...', ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-',
+    '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.', ' ': '/'
+}
+
+
+def text_to_morse(text):
+    """
+    Convert English text to Morse code
+    """
+    morse_code = ''
+    for char in text.upper():
+        if char in MORSE_CODE_DICT:
+            morse_code += MORSE_CODE_DICT[char] + ' '
+    return morse_code
+
+
+def morse_to_text(morse_code):
+    """
+    Convert Morse code to English text
+    """
+    text = ''
+    morse_code = morse_code.split(' ')
+    for code in morse_code:
+        for char, morse in MORSE_CODE_DICT.items():
+            if code == morse:
+                text += char
+                break
+    return text
+
+
